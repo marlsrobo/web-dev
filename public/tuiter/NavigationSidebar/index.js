09f36af662472d1775c6fb11c0481b6ea15ba1d3
Marley Robinson <marlsrobo@gmail.com>
2022-02-20 13:43:55 -0500

    left nav bar almost done

---
+++ public/tuiter/NavigationSidebar/index.js
@@ -0,0 +1,79 @@
+const NavigationSidebar = () => {
+    return(`
+        <div class="list-group">
+            <a class="list-group-item" href="/">
+                <i class="fab fa-twitter"></i></a>
+            <a href="home.html" class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col-2">
+                        <i class="fas fa-home"></i>
+                    </div>
+                    <div class="col-10 d-none d-xl-block">Home</div>
+                </div>
+            </a>
+            <a href="explore.html" class="list-group-item list-group-item-action active">
+                <div class="row">
+                    <div class="col-2">
+                        <i class="fas fa-hashtag"></i>
+                    </div>
+                    <div class="col-10 d-none d-xl-block">Explore</div>
+                </div>
+            </a>
+            <a href="notifications.html" class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col-2">
+                        <i class="fas fa-bell"></i>
+                    </div>
+                    <div class="col-10 d-none d-xl-block">Notifications</div>
+                </div>
+            </a>
+            <a href="messages.html" class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col-2">
+                        <i class="fas fa-envelope"></i>
+                    </div>
+                    <div class="col-10 d-none d-xl-block">Messages</div>
+                </div>
+            </a>
+            <a href="bookmarks.html" class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col-2">
+                        <i class="fas fa-bookmark"></i>
+                    </div>
+                    <div class="col-10 d-none d-xl-block">Bookmarks</div>
+                </div>
+            </a>
+            <a href="lists.html" class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col-2">
+                        <i class="fas fa-list"></i>
+                    </div>
+                    <div class="col-10 d-none d-xl-block">Lists</div>
+                </div>
+            </a>
+            <a href="profile.html" class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col-2">
+                        <i class="fas fa-user"></i>
+                    </div>
+                    <div class="col-10 d-none d-xl-block">Profile</div>
+                </div>
+            </a>
+            <a href="more.html" class="list-group-item list-group-item-action">
+                <div class="row" style="flex-wrap: nowrap; align-items: center;">
+                    <div class="col-2 fa-stack ms-0 me-0 pe-0">
+                        <i class="fas fa-circle fa-stack-1x me-0" style="font-size: 1.3em"></i>
+                        <i class="fas fa-ellipsis-h fa-stack-1x fa-inverse me-0" style="font-size: 0.7em"></i>
+                    </div>
+                    <div class="col-10 d-none d-xl-block ps-0">More</div>
+                </div>
+            </a>
+        </div>
+        <div class="d-grid mt-2">
+            <a href="tweet.html"
+                class="btn btn-primary btn-block rounded-pill">
+                Tweet</a>
+        </div>
+    `);
+}
+export default NavigationSidebar;
