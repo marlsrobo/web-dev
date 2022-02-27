d5b3238cf0d1d4e72514b32ba246aaae528cd005
Marley Robinson <marlsrobo@gmail.com>
2022-02-26 20:13:31 -0500

    a6 lab done

--- src/components/Labs/index.js
+++ src/components/Labs/index.js
@@ -1,10 +1,19 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import Classes from "./Classes";
+import Styles from "./Styles";
+import ConditionalOutput from "./ConditionalOutput";
+import TodoItem from "./Todo";
+import TodoList from "./Todo/TodoList";
 
 const Labs = () => {
     return(
         <>
             <h1>Labs</h1>
+            <Classes/>
+            <Styles/>
+            <ConditionalOutput/>
+            <TodoList/>
             <Link to="/hello">Hello</Link> |
             <Link to="/tuiter">Tuiter</Link>
         </>
